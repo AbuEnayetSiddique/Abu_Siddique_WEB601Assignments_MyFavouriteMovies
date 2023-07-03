9e2435b6dde2cf1fe83ec9ce897ab3a0d30b92ea
AbuEnayetSiddique <114108630+AbuEnayetSiddique@users.noreply.github.com>
2023-07-02 21:58:20 -0400

    Created contentDb.ts file and moved the data

---
+++ src/app/helper-files/contentDb.ts
@@ -0,0 +1,65 @@
+export const contentList=[
+    {
+      id: 1,
+      title: 'The Flowers of War',
+      description: 'Chinese-Hong Kong historical drama war film',
+      creator: 'Zhang Yimou',
+      imgURL: 'assets/img/img1.jpg',
+      type: 'History',
+      tags: ['2011']
+    },
+    {
+      id: 2,
+      title: '3 Idiots',
+      description: 'Comedy-drama film',
+      creator: 'Rajkumar Hirani',
+      imgURL: 'assets/img/img2.jpeg',
+      type: 'Comedy',
+      tags: ['2009']
+    },
+    {
+      id: 3,
+      title: 'Troy',
+      description: 'Epic historical Torjan War film',
+      creator: 'Wolfgang Petersen',
+      imgURL: '',
+      type: 'History',
+      tags: ['2004']
+    },
+    {
+      id: 4,
+      title: 'Gladiator',
+      description: 'Epic historical drama film directed by Ridley Scott and written by David Franzoni, John Logan, and William Nicholson.',
+      creator: 'Ridley Scott',
+      imgURL: 'assets/img/img4.png',
+      type: 'History',
+      tags: ['2000']
+    },
+    {
+      id: 5,
+      title: 'Ice Age',
+      description: 'Computer-animated adventure comedy film produced by Blue Sky Studios',
+      creator: 'Chris Wedge',
+      imgURL: 'assets/img/img5.jpg',
+      type: 'Comedy',
+      tags: ['2002']
+    },
+    {
+      id: 6,
+      title: 'The Notebook',
+      description: 'Romantic drama film based on the 1996 novel of the same name by Nicholas Sparks.',
+      creator: 'Nick Cassavetes',
+      imgURL: 'assets/img/img6.jpg',
+      type: '',
+      tags: ['2004']
+    },
+    {
+      id: 7,
+      title: 'Gladiator02',
+      description: 'Epic historical drama film directed by Ridley Scott and written by David Franzoni, John Logan, and William Nicholson.',
+      creator: 'Ridley Scott',
+      imgURL: 'assets/img/img4.png',
+      type: 'History, Action',
+      tags: ['2002']
+    }
+    ]
